d3407876d4afb7c89349eedb463938d7013be0e8
tkote <7146253+tkote@users.noreply.github.com>
2021-05-09 22:22:59 +0900

    trimmed

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-import json
-
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 @app.post('/call')
-async def post_call(request: Request, response: Response):
-    #print(request.url)
-    #print(request.headers)
-    #body = json.loads(await request.body())
-    body = await request.body()
-    #print(body)
+async def post_call(request: Request):
     L = []
+
     L.append('[REQUEST HEADERS]\n')
     for key in request.headers.keys():
         L.append(f'{key}: {request.headers[key]}\n')
+
+    body = await request.body()
     L.append('\n[REQUEST BODY]\n')
-    #L.append(json.dumps(body, indent=2))
     L.append(body.decode())
     L.append('\n')
+
     return PlainTextResponse(''.join(L))
